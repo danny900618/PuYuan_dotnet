15958dd60b9738882254b6ddd6a68d6fbabfd72e
趙品皓 <c0981985611@gmail.com>
2022-10-27 05:39:23 +0000

    Update PuYuanTests/Controllers/AuthControllerTests.cs

--- PuYuanTests/Controllers/AuthControllerTests.cs
+++ PuYuanTests/Controllers/AuthControllerTests.cs
@@ -19,8 +19,8 @@ namespace PuYuan.Controllers.Tests
         {
             var db_Account = "testuser";
             var db_Password = "testuser";
-            var login_Account = "testuser1";
-            var login_Password = "testuser1";
+            var login_Account = "testuser";
+            var login_Password = "testuser";
             Assert.AreEqual(db_Account, login_Account);
             Assert.AreEqual(db_Password, login_Password);
         }
